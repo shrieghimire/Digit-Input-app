b9ba3faaab2abaefdc346fc26063f496aa8ccb1e
shrieghimire <shredde111@gmail.com>
2023-03-13 15:54:18 +0545

    background image attaached

--- src/Components/DrawingBoard/DrawingBoard.js
+++ src/Components/DrawingBoard/DrawingBoard.js
@@ -1,86 +1,87 @@
-import './DrawingBoard.css';
-import {useEffect, useRef, useState} from 'react';
+import "./DrawingBoard.css";
+import { useEffect, useRef, useState } from "react";
 
 const DrawingCanvas = () => {
-    const canvasRef = useRef(null);
-    const contextRef = useRef(null);
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
 
-    const [isDrawing, setIsDrawing] = useState(false);
+  const [isDrawing, setIsDrawing] = useState(false);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        canvas.width = 32;
-        canvas.height = 32;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = 32;
+    canvas.height = 32;
 
-        const context = canvas.getContext("2d");
-        context.lineCap = "round";
-        context.strokeStyle = "black";
-        context.lineWidth = 1;
-        contextRef.current = context;
-    }, []);
+    const context = canvas.getContext("2d");
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 1.5;
+    contextRef.current = context;
+  }, []);
 
-    const startDrawing = ({nativeEvent}) => {
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.beginPath();
-        contextRef.current.moveTo(offsetX, offsetY);
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke();
-        setIsDrawing(true);
-        nativeEvent.preventDefault();
-    };
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    setIsDrawing(true);
+    nativeEvent.preventDefault();
+  };
 
-    const draw = ({nativeEvent}) => {
-        if(!isDrawing) {
-            return;
-        }
-        
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke();
-        nativeEvent.preventDefault();
-    };
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
 
-    const stopDrawing = () => {
-        contextRef.current.closePath();
-        setIsDrawing(false);
-    };
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+    nativeEvent.preventDefault();
+  };
 
-    const setToDraw = () => {
-        contextRef.current.globalCompositeOperation = 'source-over';
-    };
+  const stopDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
 
-    const setToErase = () => {
-            window.location.reload(false);
-          }
+  const setToErase = () => {
+    window.location.reload(false);
+  };
 
-    const saveImageToLocal = (event) => {
-        let link = event.currentTarget;
-        link.setAttribute('download', 'number.png');
-        let image = canvasRef.current.toDataURL('image/png');
-        link.setAttribute('href', image);
-    };
+  const saveImageToLocal = (event) => {
+    let link = event.currentTarget;
+    link.setAttribute("download", "number.png");
+    let image = canvasRef.current.toDataURL("image/png");
+    link.setAttribute("href", image);
+  };
 
-    return (
-        <>
-            <canvas className="drawing-container"
-                ref={canvasRef}
-                onMouseDown={startDrawing}
-                onMouseMove={draw}
-                onMouseUp={stopDrawing}
-                onMouseLeave={stopDrawing}>
-            </canvas>
-            <div className="buttons">
-                <button onClick={setToDraw}>
-                    Draw
-                </button>
-                <button onClick={setToErase}>Reset
-                </button>
-                <button>
-                <a id="download_image_link" href="download_link" onClick={saveImageToLocal}>Download Image</a>
-                </button>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+    <div className="canvas">
+      <canvas
+        className="drawing-container"
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseMove={draw}
+        onMouseUp={stopDrawing}
+        onMouseLeave={stopDrawing}
+      ></canvas>
+      <div className="buttons">
+        <button onClick={setToErase}>Reset</button>
+        <button>
+          <a
+            id="download_image_link"
+            href="download_link"
+            onClick={saveImageToLocal}
+          >
+            Download Image
+          </a>
+        </button>
+      </div>
+      </div>
+    </>
+  );
+};
 
 export default DrawingCanvas;
